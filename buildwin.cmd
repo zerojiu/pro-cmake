7d8638754770c55e3399888da3a0fc151eb3a7bf
lichun <lichun@cvte.com>
2016-12-04 15:15:56 +0800

    Change the CMakeLists.txt

--- buildwin.cmd
+++ buildwin.cmd
@@ -1,2 +1,25 @@
+setlocal
+
 :: add git to system environmental variables
 git submodule update --init --recursive
+
+devenv thirdparty/gRPC/vsprojects/grpc.sln /Build "Debug|x64"
+devenv thirdparty/gRPC/vsprojects/grpc.sln /Build "Release|x64"
+
+cd thirdparty/gRPC/third_party/protobuf/cmake
+mkdir win
+cd win
+cmake .. -G"Visual Studio 14 2015 Win64"
+devenv protobuf.sln /Build "Debug|x64"
+devenv protobuf.sln /Build "Release|x64"
+
+cd %~dp0
+devenv thirdparty/gRPC/vsprojects/grpc_protoc_plugins.sln /Build "Debug|x64"
+devenv thirdparty/gRPC/vsprojects/grpc_protoc_plugins.sln /Build "Release|x64"
+
+cd %~dp0
+mkdir build
+cd build
+mkdir win
+cd win
+cmake ../.. -G"Visual Studio 14 2015 Win64"
--- libs.cmake
+++ libs.cmake
@@ -20,6 +20,25 @@ IF(CMAKE_SYSTEM_NAME MATCHES "Windows")
 	 IMPORTED_LOCATION_RELEASE ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/third_party/protobuf/cmake/win/Release/libprotobuf.lib
 	 IMPORTED_LOCATION_DEBUG   ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/third_party/protobuf/cmake/win/Debug/libprotobuf.lib)
 	 
+	 #使用grpc项目通过NUGET获取的openssl库和zlib库
+	ADD_LIBRARY(eay32 SHARED IMPORTED)
+	SET_PROPERTY(TARGET eay32 PROPERTY IMPORTED_IMPLIB_DEBUG ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.1.0.204.1/build/native/lib/v140/x64/Debug/dynamic/libeay32.lib)
+	SET_PROPERTY(TARGET eay32 PROPERTY IMPORTED_LOCATION_DEBUG  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.redist.1.0.204.1/build/native/bin/v140/x64/Debug/dynamic/libeay32.dll)
+	SET_PROPERTY(TARGET eay32 PROPERTY IMPORTED_IMPLIB_RELEASE ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.1.0.204.1/build/native/lib/v140/x64/Release/dynamic/libeay32.lib)
+	SET_PROPERTY(TARGET eay32 PROPERTY IMPORTED_LOCATION_RELEASE  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.redist.1.0.204.1/build/native/bin/v140/x64/Release/dynamic/libeay32.dll)
+	
+	ADD_LIBRARY(ssleay32 SHARED IMPORTED)
+	SET_PROPERTY(TARGET ssleay32 PROPERTY IMPORTED_IMPLIB_DEBUG ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.1.0.204.1/build/native/lib/v140/x64/Debug/dynamic/ssleay32.lib)
+	SET_PROPERTY(TARGET ssleay32 PROPERTY IMPORTED_LOCATION_DEBUG  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.redist.1.0.204.1/build/native/bin/v140/x64/Debug/dynamic/ssleay32.dll)
+	SET_PROPERTY(TARGET ssleay32 PROPERTY IMPORTED_IMPLIB_RELEASE ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.1.0.204.1/build/native/lib/v140/x64/Release/dynamic/ssleay32.lib)
+	SET_PROPERTY(TARGET ssleay32 PROPERTY IMPORTED_LOCATION_RELEASE  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.openssl.redist.1.0.204.1/build/native/bin/v140/x64/Release/dynamic/ssleay32.dll)
+
+	ADD_LIBRARY(zlib SHARED IMPORTED)
+	SET_PROPERTY(TARGET zlib PROPERTY IMPORTED_IMPLIB_DEBUG ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.zlib.1.2.8.10/build/native/lib/v140/x64/Debug/dynamic/stdcall/zlib.lib)
+	SET_PROPERTY(TARGET zlib PROPERTY IMPORTED_LOCATION_DEBUG  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.zlib.redist.1.2.8.10/build/native/bin/v140/x64/Debug/dynamic/stdcall/zlib.dll)
+	SET_PROPERTY(TARGET zlib PROPERTY IMPORTED_IMPLIB_RELEASE ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.zlib.1.2.8.10/build/native/lib/v140/x64/Release/dynamic/stdcall/zlib.lib)
+	SET_PROPERTY(TARGET zlib PROPERTY IMPORTED_LOCATION_RELEASE  ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/vsprojects/packages/grpc.dependencies.zlib.redist.1.2.8.10/build/native/bin/v140/x64/Release/dynamic/stdcall/zlib.dll)
+	
 	EXECUTE_PROCESS(COMMAND  
         ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/third_party/protobuf/cmake/win/Debug/protoc.exe 
         -I ${CMAKE_SOURCE_DIR}/protos/app 
@@ -41,17 +60,5 @@ ELSEIF(CMAKE_SYSTEM_NAME MATCHES "Linux")
 	SET_TARGET_PROPERTIES(grpc++ PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/libs/opt/libgrpc++.a)	
 	SET_TARGET_PROPERTIES(protoc PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/libs/opt/protobuf/libprotoc.a)
 	SET_TARGET_PROPERTIES(protobuf PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/thirdparty/gRPC/libs/opt/protobuf/libprotobuf.a)
-
-IF(CMAKE_BUILD_TYPE MATCHES "Debug")	
-	SET_TARGET_PROPERTIES(PocoFoundation PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoFoundationd.a)
-	SET_TARGET_PROPERTIES(PocoUtil PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoUtild.a)
-	SET_TARGET_PROPERTIES(PocoXML PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoXMLd.a)	
-	SET_TARGET_PROPERTIES(PocoJSON PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoJSONd.a)
-ELSE()	
-	SET_TARGET_PROPERTIES(PocoFoundation PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoFoundation.a)
-	SET_TARGET_PROPERTIES(PocoUtil PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoUtil.a)
-	SET_TARGET_PROPERTIES(PocoXML PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoXML.a)	
-	SET_TARGET_PROPERTIES(PocoJSON PROPERTIES IMPORTED_LOCATION   ${CMAKE_SOURCE_DIR}/thirdparty/libs/lib/libPocoJSON.a)
-ENDIF(CMAKE_BUILD_TYPE MATCHES "Debug")
 	
 ENDIF(CMAKE_SYSTEM_NAME MATCHES "Windows")
